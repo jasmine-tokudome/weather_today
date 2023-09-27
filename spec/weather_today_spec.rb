30a7ac31f596b67ea93c0b1b01af637745640218
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-09-27 16:10:27 +0900

    RuboCopの警告を解決するため再修正。

--- spec/weather_today_spec.rb
+++ spec/weather_today_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe WeatherToday::WeatherClient do
   end
 
   context 'when fetching weather data for kagoshima' do
+    subject(:weather_data) { WeatherToday::WeatherClient.new('kagoshima').fetch_weather_data }
+
     before do
       stub_request(:get, /api\.open-meteo\.com/).to_return(
         status: 200,
@@ -28,28 +30,13 @@ RSpec.describe WeatherToday::WeatherClient do
     end
 
     it 'returns weather data with correct latitude and longitude' do
-      # テスト対象のWeatherClientを作成
-      weather_client = WeatherToday::WeatherClient.new('kagoshima')
-
-      # テスト実行
-      weather_data = weather_client.fetch_weather_data
-
-      # HTTPステータスコードが成功 (200) であることを確認
-      expect(weather_data).to be_a(Hash) # weather_dataがハッシュであることを確認
+      expect(weather_data).to be_a(Hash)
     end
 
     it 'includes required keys in the response' do
-      weather_client = WeatherToday::WeatherClient.new('kagoshima')
-      weather_data = weather_client.fetch_weather_data
-
-      # レスポンスの中身を詳細に検証する
-      expect(weather_data).to have_key('UV指数')
-      expect(weather_data).to have_key('天気')
-      expect(weather_data).to have_key('日の入り時刻')
-      expect(weather_data).to have_key('日の出時刻')
-      expect(weather_data).to have_key('日時')
-      expect(weather_data).to have_key('最低気温')
-      expect(weather_data).to have_key('最高気温')
+      expect(weather_data).to include(
+        'UV指数', '天気', '日の入り時刻', '日の出時刻', '日時', '最低気温', '最高気温'
+      )
     end
   end
 end
