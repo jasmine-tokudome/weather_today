47bff8b21e47942a79107b94df9db7b74497b58a
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-05-26 23:21:59 +0900

    2024_5_26(イベントパラメーターを理解する)

--- react-typescript-weather-app/src/components/Form.tsx
+++ react-typescript-weather-app/src/components/Form.tsx
@@ -5,6 +5,7 @@ const Form = () => {
     return(
 <form>
     <input type="text" name="city" placeholder="都市名" onChange = {e => setCity(e.target.value)}/>
+    {city}
     <button type="submit">Get Weather</button>
 </form>
     );
