ec348933d7ad2fabc36d2207b1446d66ac663fc2
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-05-27 14:14:38 +0900

    2024_5_27(APIコール)

--- react-typescript-weather-app/src/components/Form.tsx
+++ react-typescript-weather-app/src/components/Form.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
 const Form = () => {
-    const [city,setCity] = useState("");
+    const [city,setCity] = useState<string>("");
+    const getWeather = () => {
+        fetch("http://api.weatherapi.com/v1/current.json?key=cb54494cc2a845f3b36145131240302&q=London&aqi=no
+        ")
+    }
     return(
 <form>
     <input type="text" name="city" placeholder="都市名" onChange = {e => setCity(e.target.value)}/>
