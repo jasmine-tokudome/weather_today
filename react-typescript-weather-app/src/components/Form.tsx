601654dd8718798646943a3c651fcc9b970845f5
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-05-28 23:27:32 +0900

    2024_5_28(APIから受け取ったデータをJSONで受け取る)

--- react-typescript-weather-app/src/components/Form.tsx
+++ react-typescript-weather-app/src/components/Form.tsx
@@ -5,7 +5,8 @@ const Form = () => {
     const getWeather = (e: any) => {
         e.preventDefault();
         fetch("http://api.weatherapi.com/v1/current.json?key=cb54494cc2a845f3b36145131240302&q=London&aqi=no")
-        .then(res => console.log(res))
+        .then(res => res.json())
+        .then(data => console.log(data))
     }
     return(
 <form>
