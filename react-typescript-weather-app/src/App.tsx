b402a528d678c469f225ab0b96116d240c510aa6
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-05-30 17:09:55 +0900

    2024_5_30（データをAppにおく）

--- react-typescript-weather-app/src/App.tsx
+++ react-typescript-weather-app/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
 import './App.css';
 
 function App() {
+  const [city,setCity] = useState<string>("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=cb54494cc2a845f3b36145131240302&q=London&aqi=no")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
   return (
     <div className="test">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather}/>
       <Results />
     </div>
   );
